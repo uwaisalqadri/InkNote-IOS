8fea49d32d35d317e61061eca4413a673d387a76
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-04-24 05:34:26 +0800

    add custom font

--- InkNote-IOS/App/InkNoteApp.swift
+++ InkNote-IOS/App/InkNoteApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InkNoteApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
--- InkNote-IOS/Core/Model/Note.swift
+++ InkNote-IOS/Core/Model/Note.swift
@@ -6,12 +6,17 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct Note: Identifiable {
-    let id = UUID()
-    let title: String
-    let description: String
-    let date: String
-    let isRemove: Bool
-    let imageUrl: String
+public class Note: Object {
+    @objc dynamic var id: Int = 0
+    @objc dynamic var title: String = ""
+    @objc dynamic var desc: String = ""
+    @objc dynamic var date: String = ""
+    @objc dynamic var isRemove: Bool = false
+    @objc dynamic var imageUrl: String = ""
+    
+    public override class func primaryKey() -> String? {
+        return "id"
+    }
 }
