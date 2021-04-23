4d424fc9546d65a163b40ba6422d488852173e00
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-04-24 06:02:58 +0800

    add title and menu button

--- InkNote-IOS/Module/Home/HomeView.swift
+++ InkNote-IOS/Module/Home/HomeView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, World!")
-            .font(.custom("Poppins-SemiBold", size: 22))
+        NavigationView {
+            Text("Hello")
+                .navigationBarItems(leading: menuButton)
+        }
+    }
+}
+
+
+extension HomeView {
+    
+    
+    var menuButton: some View {
+        VStack {
+            Spacer(minLength: 45)
+            HStack {
+                Button(action: {
+                    print("Button menu")
+                }, label: {
+                    Image("MenuIconLight")
+                        .foregroundColor(.black)
+                        .frame(width: 41, height: 41, alignment: .center)
+                })
+                .foregroundColor(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 2)
+                        .shadow(color: .black, radius: 2, x: 0, y: 4)
+                        .foregroundColor(.white)
+                )
+                
+                Text("InkNote")
+                    .font(.custom("Poppins-Bold", size: 30))
+            }
+        }
     }
 }
 
