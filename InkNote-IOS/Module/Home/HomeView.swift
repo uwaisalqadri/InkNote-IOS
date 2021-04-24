2bb1740faa820289a748f0922f7d5adb223688f6
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-04-24 11:27:43 +0800

    expandable fab

--- InkNote-IOS/Module/Home/HomeView.swift
+++ InkNote-IOS/Module/Home/HomeView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var show = false
+    
     var body: some View {
         NavigationView {
-            Text("Hello")
+            ZStack {
+                Text("Hello world")
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        ExpandableFab(show: $show)
+                    }.padding([.leading, .trailing], 30)
+                }
+            }
                 .navigationBarItems(leading: menuButton)
         }
     }
@@ -27,22 +39,104 @@ extension HomeView {
                 Button(action: {
                     print("Button menu")
                 }, label: {
-                    Image("MenuIconLight")
-                        .foregroundColor(.black)
-                        .frame(width: 41, height: 41, alignment: .center)
+                    VStack {
+                        Image("MenuIconLight")
+                            .frame(width: 41, height: 41, alignment: .center)
+                    }.background(Color.white)
                 })
-                .foregroundColor(.white)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.black, lineWidth: 2)
                         .shadow(color: .black, radius: 2, x: 0, y: 4)
                         .foregroundColor(.white)
                 )
-                
+                .background(Color.white)
                 Text("InkNote")
                     .font(.custom("Poppins-Bold", size: 30))
             }
         }
+    }
+}
+
+struct ExpandableFab: View {
+    
+    @Binding var show: Bool
+    
+    var body: some  View {
+        VStack {
+            
+            if self.show {
+                Button(action: {
+                    // navigate to write view
+                }) {
+                    Image("WriteIcon")
+                        .frame(width: 10, height: 10)
+                        .padding(22)
+                }
+                .background(Color(red: 0.003, green: 0.85, blue: 0.774))
+                .foregroundColor(.black)
+                .clipShape(Circle())
+                .shadow(radius: 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color.black, lineWidth: 2)
+                        .foregroundColor(.white)
+                )
+                
+                Button(action: {
+                    // insert link
+                }) {
+                    Image("LinkIcon")
+                        .frame(width: 10, height: 10)
+                        .padding(22)
+                }
+                .background(Color(red: 0.003, green: 0.85, blue: 0.774))
+                .foregroundColor(.black)
+                .clipShape(Circle())
+                .shadow(radius: 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color.black, lineWidth: 2)
+                        .foregroundColor(.white)
+                )
+                
+                Button(action: {
+                    // get image
+                }) {
+                    Image("ImageIcon")
+                        .frame(width: 10, height: 10)
+                        .padding(22)
+                }
+                .background(Color(red: 0.003, green: 0.85, blue: 0.774))
+                .foregroundColor(.black)
+                .clipShape(Circle())
+                .shadow(radius: 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color.black, lineWidth: 2)
+                        .foregroundColor(.white)
+                )
+            }
+            
+            Button(action: {
+                self.show.toggle()
+            }) {
+                Image("AddIcon")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                    .padding(22)
+            }
+            .background(Color(red: 0.003, green: 0.85, blue: 0.774))
+            .foregroundColor(.black)
+            .clipShape(Circle())
+            .shadow(radius: 8)
+            .rotationEffect(.init(degrees: self.show ? -45 : 0))
+            .overlay(
+                RoundedRectangle(cornerRadius: 40)
+                    .stroke(Color.black, lineWidth: 2)
+                    .foregroundColor(.white)
+            )
+        }.animation(.spring())
     }
 }
 
